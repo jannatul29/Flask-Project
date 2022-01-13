2c6f90788614178dfdd68e975b0fd1e30a51624e
jannatul29 <2017-2-60-029@std.ewubd.bd>
2022-01-13 16:17:59 +0600

    swagger complete

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template, request, redirect, url_for , jsonify
+import argparse
+import os
+from flask import Flask, jsonify, make_response
+from flask_cors import CORS
+from flask_swagger_ui import get_swaggerui_blueprint
+#from routes import request_api
+from flask import jsonify, abort, request, Blueprint
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from sqlalchemy import desc
-from flask_swagger_ui import get_swaggerui_blueprint
+from sqlalchemy import desc, and_, or_, not_
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:root@localhost:5432/store"
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+APP = Flask(__name__)
+
+APP.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:root@localhost:5432/store"
+db = SQLAlchemy(APP)
+migrate = Migrate(APP, db)
 
 class CarsModel(db.Model):
     __tablename__ = 'test12'
@@ -36,7 +42,23 @@ class CarsModel(db.Model):
     def __repr__(self):
         return f"<Car {self.title}>"
 
-@app.route('/')
+### swagger specific ###
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Seans-Python-Flask-REST-Boilerplate"
+    }
+)
+APP.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+### end swagger specific ###
+
+
+#APP.register_blueprint(request_api.get_blueprint())
+
+@APP.route('/')
 def home_route():
     hotels = CarsModel.query.all()
     output = []
@@ -52,7 +74,24 @@ def home_route():
         output.append(data)
     return jsonify(output)
 
-@app.route('/name', methods=['GET'])
+# @APP.route('/name=<string:title>', methods=['GET'])
+# def get_product(title):
+#     hotel2 = CarsModel.query.filter_by(title=title).all()
+#     output2 = []
+#     for hotel in hotel2:
+#         data2 = {}
+#         data2['amentites'] = hotel.amenities
+#         data2['location'] = hotel.location
+#         data2['price'] = hotel.price
+#         data2['rating'] = hotel.rating
+#         data2['image'] = hotel.image
+#         data2['herf'] = hotel.herf
+#         data2['title'] = hotel.title
+#         output2.append(data2)
+#     return jsonify(output2)
+
+
+@APP.route('/name', methods=['GET'])
 def get_product():
     title = request.args.get('title')
     hotel2 = CarsModel.query.filter_by(title=title).all()
@@ -69,7 +108,7 @@ def get_product():
         output2.append(data2)
     return jsonify(output2)
 
-@app.route('/place', methods=['GET'])
+@APP.route('/place', methods=['GET'])
 def get_product1():
     location = request.args.get('location')
     hotel3 = CarsModel.query.filter_by(location=location).all()
@@ -86,7 +125,7 @@ def get_product1():
         output3.append(data3)
     return jsonify(output3)
 
-@app.route('/price', methods=['GET'])
+@APP.route('/price', methods=['GET'])
 def get_product2():
     sorting = request.args.get('sorting')
     if sorting == 'asc':
@@ -106,6 +145,84 @@ def get_product2():
         output4.append(data4)
     return jsonify(output4)
 
+@APP.route('/search', methods=['GET'])
+def search():
+    args = request.args
+    title = args.get('title')
+    location = args.get('location')
+    amenities = args.get('amenities')
+    search = "%{}%".format(amenities)
+    sorting = args.get('sorting')
 
+    # result = db_users
+    if None not in (title, location):
+        hotel2 = CarsModel.query.filter_by(title=title, location=location).all()
+    elif title is not None:
+        hotel2 = CarsModel.query.filter_by(title=title).all()
+    elif location is not None:
+        hotel2 = CarsModel.query.filter_by(location=location).all()
+    elif amenities is not None:
+        hotel2 = CarsModel.query.filter(CarsModel.amenities.like(search)).all()
+    elif sorting == 'asc' and sorting is not None:
+        hotel2 = CarsModel.query.order_by(CarsModel.price).all()
+    elif sorting == 'dsc' and sorting is not None:
+        hotel2 = CarsModel.query.order_by(CarsModel.price.desc()).all()
+    elif None in (title, location):
+        hotel2 = CarsModel.query.all()
+    output2 = []
+    for hotel in hotel2:
+        data2 = {}
+        data2['amentites'] = hotel.amenities
+        data2['location'] = hotel.location
+        data2['price'] = hotel.price
+        data2['rating'] = hotel.rating
+        data2['image'] = hotel.image
+        data2['herf'] = hotel.herf
+        data2['title'] = hotel.title
+        output2.append(data2)
+    return jsonify(output2)
+
+
+@APP.errorhandler(400)
+def handle_400_error(_error):
+    """Return a http 400 error to client"""
+    return make_response(jsonify({'error': 'Misunderstood'}), 400)
+
+
+@APP.errorhandler(401)
+def handle_401_error(_error):
+    """Return a http 401 error to client"""
+    return make_response(jsonify({'error': 'Unauthorised'}), 401)
+
+
+@APP.errorhandler(404)
+def handle_404_error(_error):
+    """Return a http 404 error to client"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@APP.errorhandler(500)
+def handle_500_error(_error):
+    """Return a http 500 error to client"""
+    return make_response(jsonify({'error': 'Server error'}), 500)
+
+
+# if __name__ == '__main__':
+
+#     PARSER = argparse.ArgumentParser(
+#         description="Seans-Python-Flask-REST-Boilerplate")
+
+#     PARSER.add_argument('--debug', action='store_true',
+#                         help="Use flask debug/dev mode with file change reloading")
+#     ARGS = PARSER.parse_args()
+
+#     PORT = int(os.environ.get('PORT', 5000))
+
+#     if ARGS.debug:
+#         print("Running in debug mode")
+#         CORS = CORS(APP)
+#         APP.run(host='0.0.0.0', port=PORT, debug=True)
+#     else:
+#         APP.run(host='0.0.0.0', port=PORT, debug=False)
 if __name__ == '__main__':
-    app.run(debug=True)
+    APP.run(debug=True)
--- test.py
+++ test.py
@@ -3,24 +3,94 @@ import os
 from flask import Flask, jsonify, make_response
 from flask_cors import CORS
 from flask_swagger_ui import get_swaggerui_blueprint
-from routes import request_api
+#from routes import request_api
+from flask import jsonify, abort, request, Blueprint
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from sqlalchemy import desc, and_, or_, not_
 
 APP = Flask(__name__)
 
+APP.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:root@localhost:5432/store"
+db = SQLAlchemy(APP)
+migrate = Migrate(APP, db)
+
+class store(db.Model):
+    __tablename__ = 'test12'
+
+    id = db.Column(db.INTEGER, primary_key=True)
+    amenities = db.Column(db.String())
+    location = db.Column(db.String())
+    price = db.Column(db.String())
+    rating = db.Column(db.String())
+    image = db.Column(db.String())
+    herf = db.Column(db.String())
+    title = db.Column(db.String())
+
+
+
+    def __init__(self, title, herf, image, rating, price, location, amenities):   
+        self.amenities = amenities
+        self.location = location
+        self.price = price
+        self.rating = rating
+        self.image = image
+        self.herf = herf
+        self.title = title
+
+
+    def __repr__(self):
+        return f"<Car {self.title}>"
+
 ### swagger specific ###
 SWAGGER_URL = '/swagger'
-#API_URL = '/static/swagger.json'
+API_URL = '/static/swagger.json'
 SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
     SWAGGER_URL,
+    API_URL,
     config={
         'app_name': "Seans-Python-Flask-REST-Boilerplate"
     }
 )
 APP.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
-### end swagger specific ###
 
 
-APP.register_blueprint(request_api.get_blueprint())
+@APP.route('/search', methods=['GET'])
+def search():
+    args = request.args
+    title = args.get('title')
+    location = args.get('location')
+    amenities = args.get('amenities')
+    search = "%{}%".format(amenities)
+    sorting = args.get('sorting')
+
+    # result = db_users
+    if None not in (title, location):
+        hotel2 = store.query.filter_by(title=title, location=location).all()
+    elif title is not None:
+        hotel2 = store.query.filter_by(title=title).all()
+    elif location is not None:
+        hotel2 = store.query.filter_by(location=location).all()
+    elif amenities is not None:
+        hotel2 = store.query.filter(store.amenities.like(search)).all()
+    elif sorting == 'asc' and sorting is not None:
+        hotel2 = store.query.order_by(store.price).all()
+    elif sorting == 'dsc' and sorting is not None:
+        hotel2 = store.query.order_by(store.price.desc()).all()
+    elif None in (title, location):
+        hotel2 = store.query.all()
+    output2 = []
+    for hotel in hotel2:
+        data2 = {}
+        data2['amentites'] = hotel.amenities
+        data2['location'] = hotel.location
+        data2['price'] = hotel.price
+        data2['rating'] = hotel.rating
+        data2['image'] = hotel.image
+        data2['herf'] = hotel.herf
+        data2['title'] = hotel.title
+        output2.append(data2)
+    return jsonify(output2)
 
 
 @APP.errorhandler(400)
@@ -47,20 +117,22 @@ def handle_500_error(_error):
     return make_response(jsonify({'error': 'Server error'}), 500)
 
 
+# if __name__ == '__main__':
+
+#     PARSER = argparse.ArgumentParser(
+#         description="Seans-Python-Flask-REST-Boilerplate")
+
+#     PARSER.add_argument('--debug', action='store_true',
+#                         help="Use flask debug/dev mode with file change reloading")
+#     ARGS = PARSER.parse_args()
+
+#     PORT = int(os.environ.get('PORT', 5000))
+
+#     if ARGS.debug:
+#         print("Running in debug mode")
+#         CORS = CORS(APP)
+#         APP.run(host='0.0.0.0', port=PORT, debug=True)
+#     else:
+#         APP.run(host='0.0.0.0', port=PORT, debug=False)
 if __name__ == '__main__':
-
-    PARSER = argparse.ArgumentParser(
-        description="Seans-Python-Flask-REST-Boilerplate")
-
-    PARSER.add_argument('--debug', action='store_true',
-                        help="Use flask debug/dev mode with file change reloading")
-    ARGS = PARSER.parse_args()
-
-    PORT = int(os.environ.get('PORT', 5000))
-
-    if ARGS.debug:
-        print("Running in debug mode")
-        CORS = CORS(APP)
-        APP.run(host='0.0.0.0', port=PORT, debug=True)
-    else:
-        APP.run(host='0.0.0.0', port=PORT, debug=False)
+    APP.run(debug=True)
